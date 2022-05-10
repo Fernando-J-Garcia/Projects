737350e4a7929e1366d5d216375ce78c848114c3
Fernando-J-Garcia <105046544+Fernando-J-Garcia@users.noreply.github.com>
2022-05-10 03:01:00 -0700

    Add files via upload

---
+++ SimpleDialogApp/DialogAppTest.cpp
@@ -0,0 +1,166 @@
+
+// DialogAppTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#define NOMINMAX
+#include <Windows.h>
+#include <limits>
+using namespace std;
+
+void coutColor(string inputString, WORD colorCode);
+class Dialog {
+public:
+    class Decision {
+    public:
+        Decision();
+        Decision(string decision, string response);
+        ~Decision();
+        string decision;
+        string response;        
+    };
+
+    Dialog();
+    Dialog(string speaker, string text, vector<Decision> decisions);
+    ~Dialog();
+    string speaker;
+    string text;
+    vector<Decision> decisions;
+};
+
+int main()
+{
+    //This is where your dialog data will be stored. It would be best practice to put it in a seperate class and file, but for the sake of
+    //simplicity and jessie's small brain it will go in here.
+    vector<Dialog> dialogData;
+    Dialog dialog;
+    string speaker = "";
+    string text = "";
+    vector<Dialog::Decision> decisions;
+
+    //Person that is going to be speaking... the name is used for display purposes only
+    //EX:(Bob: dialog here...)
+    speaker = "bob"; 
+    //This is the dialog the speaker is going to say.
+    text = "Hi my name is bob!";
+    //Clear the decision array from the previous dialog.
+    decisions.clear();
+    //If there are any decisions that need to be made with this dialog then put them here
+    //otherwise this SHOULD be empty! Empty is what tells the program to not run any of
+    //the logic assosiated with decision dialogs.
+    decisions = vector<Dialog::Decision>{
+        Dialog::Decision("decision here","response if i made that decision"),
+        Dialog::Decision("decision here","response if i made that decision"),
+        Dialog::Decision("decision here","response if i made that decision"),
+        Dialog::Decision("decision here","response if i made that decision"),
+    };
+    //Make a new dialog with the properties we declared above...
+    dialog = Dialog(speaker, text, decisions);
+    //Finally we store the dialog...
+    dialogData.push_back(dialog);
+
+    //Go through each piece of dialog and display it. 
+    for (Dialog d: dialogData) {
+        //Display dialog
+        cout << d.speaker << ": " << d.text << "\n";
+        //If we have no decisions to make then skip the
+        //decision logic!
+        if (d.decisions.empty() == false) {
+            //-1 means they didn't answer which should probably means something went wrong..
+            int decisionMade = -1;
+            for (int j = 0; j < d.decisions.size();j++) {
+                //Write the decisions the user can choose between
+                cout << j << ": " << d.decisions[j].decision << "\n";
+            }
+            //Tell the user to type a decision
+            cout << "Type in the a number to respond ";
+            cin >> decisionMade;
+            //Check for invalid inputs
+            while (cin.fail() || decisionMade > d.decisions.size()-1 || decisionMade < 0)
+            {
+                //Clear the input
+                cin.clear();
+                //Tells cin to ignore all the input on this line
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                coutColor("Invalid Entry Type in a number! ",12);
+                cin >> decisionMade;
+            }
+
+            //If get here that means we have put in a valid input
+            cout << d.speaker << ": " << d.decisions.at(decisionMade).response;
+        }
+    }
+#pragma region Forloop version if thats better for you
+    /*for (int i = 0; i < dialogData.size(); i++) {
+        cout << dialogData[i].speaker << ": " << dialogData[i].text << "\n";
+        if (dialogData[i].decisions.empty() == false) {
+            for (int j = 0; j < dialogData[i].decisions.size(); j++) {
+                cout << dialogData[i].decisions[j] << "\n";
+            }
+        }
+    }*/
+#pragma endregion
+
+    cout << "\n============================================\n";
+    cout << "End of our program no more dialog!\n";
+}
+
+Dialog::Dialog()
+{
+    Dialog::speaker = "";
+    Dialog::text = "";
+}
+
+Dialog::Dialog(string speaker, string text, vector<Decision> decisions)
+{
+    Dialog::speaker = speaker;
+    Dialog::text = text;
+    Dialog::decisions = decisions;
+}
+
+Dialog::~Dialog()
+{
+}
+
+Dialog::Decision::Decision()
+{
+}
+
+Dialog::Decision::Decision(string decision, string response)
+{
+    Decision::decision = decision;
+    Decision::response = response;
+}
+
+Dialog::Decision::~Decision()
+{
+}
+//Takes in ANSCII code for the color and writes the input string in that color
+//The Console color gets reset back to the original color
+#pragma region COLORCODES
+//10 = LIMEGREEN
+//11 = CYAN
+//12 = RED
+//13 = MAGENTA
+//14 = YELLOW
+//15 = WHITE
+#pragma endregion
+void coutColor(string inputString, WORD colorCode)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO info;
+
+    //If we couldn't get screen buffer info the BAIL!
+    if (!GetConsoleScreenBufferInfo(hConsole, &info))return;
+    
+    //Store the previous color in a variable 
+    short prevColor = info.wAttributes;
+
+    //Write the input text
+    SetConsoleTextAttribute(hConsole, colorCode);
+    cout << inputString;
+    //Reset the color...
+    SetConsoleTextAttribute(hConsole, prevColor);
+}
